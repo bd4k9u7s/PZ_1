021449904d11f8092c88207e991d41190325b1f6
bd4k9u7s <62236467+bd4k9u7s@users.noreply.github.com>
2020-05-20 15:56:01 +0300

    task_01_4O-506C_Kudryavtseva_02.py
    Second

--- task_01_4O-506C_Kudryavtseva_02.py
+++ task_01_4O-506C_Kudryavtseva_02.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-try: os.mkdir('results')
-except OSError: pass
+if not os.path.exists('results'):
+    os.mkdir('results')
+    
 x = np.linspace(-5,5,1000)
 y = -20 * np.exp(-0.2 * np.sqrt(0.5 * x**2)) - np.exp(0.5 *
                 (np.cos(2 * np.pi * x) + 1)) + np.e + 20
@@ -13,10 +14,13 @@ plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid()
 plt.show()
+
+p = os.path.join(os.getcwd(),'results','task_01_4O-506C_Kudryavtseva_02.txt')
     
-with open('results/task_01_4O-506C_Kudryavtseva_02.txt', 'w') as file:
-    file.write('x\ty(x)\n')
+with open(p, 'w') as file:
+    file.write('x{0:4}y(x)\n'.format(' '))
     for i in range(0,1000):
-        file.write(str(x[i])+'\t'+str(y[i])+'\n')          
+        file.write('{0}{1:4}{2}\n'.format(str(x[i]),' ',str(y[i])))
+       
         
     
